58e61ecb209f92fc9fbf5420409adef8b6a99a49
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2026-01-24 00:18:48 -0800

    minor keyword error

--- leetcode/src/app/api/GenerateLeetCode/route.js
+++ leetcode/src/app/api/GenerateLeetCode/route.js
@@ -1,6 +1,7 @@
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+const ai = new GoogleGenerativeAI ({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function POST(req) {
   try {
